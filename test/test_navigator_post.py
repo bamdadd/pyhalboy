1299d1aef894e31d5e0dd2f6d182dd4426424aa4
Toby Clemson <tobyclemson@gmail.com>
2024-12-14 22:25:27 +0000

    Add precommit task. Fix linting issues.

--- test/test_navigator_post.py
+++ test/test_navigator_post.py
@@ -6,31 +6,41 @@ from pyhalboy.navigator import Navigator
 
 
 def createUser(id, name):
-    return Resource() \
-        .add_link('self', '/users/{}'.format(id)) \
-        .add_property('name', name)
+    return (
+        Resource()
+        .add_link("self", "/users/{}".format(id))
+        .add_property("name", name)
+    )
 
 
 class NavigatorPostTestCase(unittest.TestCase):
     def test_post(self):
         with requests_mock.Mocker() as m:
-            m.get('http://test.com', json={"_links":
-                                               {"users": {"href": "/users"}},
-                                           "_embedded": {},
-                                           "prop1": 1})
+            m.get(
+                "http://test.com",
+                json={
+                    "_links": {"users": {"href": "/users"}},
+                    "_embedded": {},
+                    "prop1": 1,
+                },
+            )
 
-            m.post('http://test.com/users', json={
-                'name': 'Thomas'
-            }, headers={"Location": "http://test.com/users/thomas"})
+            m.post(
+                "http://test.com/users",
+                json={"name": "Thomas"},
+                headers={"Location": "http://test.com/users/thomas"},
+            )
             navigator = Navigator()
-            headers = {
-                "authorization": 'some-token'
-            }
-            discovery_result = navigator.discover('http://test.com', {"http": {"headers": headers}})
-            result = discovery_result.post('users', {'name': 'Thomas'})
+            headers = {"authorization": "some-token"}
+            discovery_result = navigator.discover(
+                "http://test.com", {"http": {"headers": headers}}
+            )
+            result = discovery_result.post("users", {"name": "Thomas"})
             self.assertEqual(result.status(), 200)
-            self.assertEqual(result.get_header('Location'), "http://test.com/users/thomas")
+            self.assertEqual(
+                result.get_header("Location"), "http://test.com/users/thomas"
+            )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
